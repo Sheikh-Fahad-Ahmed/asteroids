fca31c25a5052d8e90ff3b313c181e973850e0a7
Fahad Ahmed <fahad838@gmail.com>
2025-02-17 13:05:34 +0530

    Refactor the code to use Groups

--- main.py
+++ main.py
@@ -10,14 +10,28 @@ def main():
     dt = 0
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers(updatable, drawable)
+
     player = Player(x, y)
+
+    
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        updatable.update(dt)
         screen.fill(color="black")
-        player.draw(screen)
+        
+        for obj in drawable:
+            obj.draw(screen)
+        
         pygame.display.flip()
         
         dt = clock.tick(60) / 1000
