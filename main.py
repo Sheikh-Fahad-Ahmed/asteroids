a3303e2ce3fa31fa668c0eb9674e2d12aac95174
Fahad Ahmed <fahad838@gmail.com>
2025-02-18 11:34:27 +0530

    Add split method for asteroids

--- main.py
+++ main.py
@@ -41,7 +41,8 @@ def main():
 
             for shot in shots:
                 if shot.check_collision(asteroid):
-                    asteroid.kill()
+                    shot.kill()
+                    asteroid.split()
 
         screen.fill(color="black")
 
